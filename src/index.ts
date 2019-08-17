aabc3f801b3bc821c5fec2e6257d20e689022bb1
sswq2020 <a309108243@qq.com>
2019-08-17 08:36:23 +0800

    feat: axios add buildURL Function

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,27 @@
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
+import { buildURL } from './helpers/url'
 
+/**
+ * @description axios函数的主体,调用xhr函数,实现浏览器ajax功能
+ * @param {AxiosRequestConfig} config
+ */
 function axios(config: AxiosRequestConfig): void {
+  processConfig(config)
   xhr(config)
+}
+
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformURL(config)
+}
+
+/**
+ * @description 调用buildURL函数,对utl进行处理
+ * @param {AxiosRequestConfig} config
+ */
+function transformURL(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildURL(url, params)
 }
 
 export default axios
