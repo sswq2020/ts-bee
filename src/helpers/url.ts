7fa44f436b9aed7988a933bd2da2ceb9c3266fb2
sswq2020 <a309108243@qq.com>
2019-08-17 08:29:43 +0800

    feat: handle kinds of params feature and join url

---
+++ src/helpers/url.ts
@@ -0,0 +1,65 @@
+import { isDate, isObject } from './util'
+
+/**
+ * @author sswq
+ * @param {string} val 传入的字符串
+ * @function 对特殊的:$,+[]进行encodeURIComponent编码"
+ * @returns {string}
+ */
+function encode(val: string): string {
+  return encodeURIComponent(val)
+    .replace(/%40/g, '@')
+    .replace(/%3A/gi, ':')
+    .replace(/%24/g, '$')
+    .replace(/%2C/gi, ',')
+    .replace(/%20/g, '+')
+    .replace(/%5B/gi, '[')
+    .replace(/%5D/gi, ']')
+}
+/**
+ * @function 处理请求的url和params,形成url?key1=v1&key2=v2的请求方式
+ * @param {string} url 请求地址
+ * @param {Object} params 任意类型参数
+ *
+ */
+export function buildURL(url: string, params: any): string {
+  if (!params) {
+    return url
+  }
+
+  let parts: Array<String> = []
+  Object.keys(params).forEach(key => {
+    let val = params[key]
+    // 当value不存在是直接跳过
+    if (val === null || val === undefined) {
+      return
+    }
+
+    let values = []
+    if (Array.isArray(val)) {
+      key = key + '[]'
+      values = val
+    } else {
+      values = [val]
+    }
+
+    values.forEach(item => {
+      if (isDate(item)) {
+        item = item.toISOString()
+      } else if (isObject(item)) {
+        item = JSON.stringify(item)
+      }
+      parts.push(`${encode(key)}=${encode(item)}`)
+    })
+  })
+
+  let serimal = parts.join('&')
+  if (serimal) {
+    let marIndex: number = url.indexOf('#')
+    if (marIndex > -1) {
+      url = url.slice(0, marIndex)
+    }
+    url = url + (url.indexOf('?') === -1 ? '?' + serimal : '&' + serimal)
+  }
+  return url
+}
