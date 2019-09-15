35e0ace66181e0339f48eb3abc03e9664c154158
sswq2020 <a309108243@qq.com>
2019-09-15 17:03:12 +0800

    feat: xhr Function return AxiosPromise and xhr add onreadystatechange callback

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,18 +1,43 @@
-import { AxiosRequestConfig } from './types'
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
 
-function xhr(config: AxiosRequestConfig): void {
-  const { url, method = 'get', data = null, headers = {} } = config
-  const request = new XMLHttpRequest()
-  request.open(method.toLocaleUpperCase(), url, true)
-  Object.keys(headers).forEach(name => {
-    if (data === null && name.toLocaleLowerCase() === 'content-type') {
-      delete headers[name]
-    } else {
-      request.setRequestHeader(name, headers[name])
+function xhr(config: AxiosRequestConfig): AxiosPromise {
+  return new Promise((resolve, reject) => {
+    const { url, method = 'get', data = null, headers = {}, responseType } = config
+    const request = new XMLHttpRequest()
+
+    if (responseType) {
+      request.responseType = responseType
     }
-  })
 
-  request.send(data)
+    request.open(method.toLocaleUpperCase(), url, true)
+    request.onreadystatechange = function() {
+      // 查阅MDN文档 https://developer.mozilla.org/zh-CN/docs/Web/API/XMLHttpRequest/onreadystatechange
+      if (request.readyState !== 4) {
+        return
+      }
+      const responseHeaders = request.getAllResponseHeaders()
+      const responseData = responseType !== 'text' ? request.response : request.responseText
+      const response: AxiosResponse = {
+        data: responseData,
+        status: request.status,
+        statusText: request.statusText,
+        headers: responseHeaders,
+        config,
+        request
+      }
+      resolve(response)
+    }
+
+    Object.keys(headers).forEach(name => {
+      if (data === null && name.toLocaleLowerCase() === 'content-type') {
+        delete headers[name]
+      } else {
+        request.setRequestHeader(name, headers[name])
+      }
+    })
+
+    request.send(data)
+  })
 }
 
 export default xhr
