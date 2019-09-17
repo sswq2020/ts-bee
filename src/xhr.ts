a853a592c161f872a1d8dc44e5d130f36b3c64f2
sswq2020 <a309108243@qq.com>
2019-09-17 19:58:48 +0800

    perf: request ---->XHR can better understand

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,29 +1,30 @@
 import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
+import { parseHeaders } from './helpers/headers'
 
 function xhr(config: AxiosRequestConfig): AxiosPromise {
   return new Promise((resolve, reject) => {
     const { url, method = 'get', data = null, headers = {}, responseType } = config
-    const request = new XMLHttpRequest()
+    const XHR = new XMLHttpRequest()
 
     if (responseType) {
-      request.responseType = responseType
+      XHR.responseType = responseType
     }
 
-    request.open(method.toLocaleUpperCase(), url, true)
-    request.onreadystatechange = function() {
+    XHR.open(method.toLocaleUpperCase(), url, true)
+    XHR.onreadystatechange = function() {
       // 查阅MDN文档 https://developer.mozilla.org/zh-CN/docs/Web/API/XMLHttpRequest/onreadystatechange
-      if (request.readyState !== 4) {
+      if (XHR.readyState !== 4) {
         return
       }
-      const responseHeaders = request.getAllResponseHeaders()
-      const responseData = responseType !== 'text' ? request.response : request.responseText
+      const responseHeaders = parseHeaders(XHR.getAllResponseHeaders())
+      const responseData = responseType !== 'text' ? XHR.response : XHR.responseText
       const response: AxiosResponse = {
         data: responseData,
-        status: request.status,
-        statusText: request.statusText,
+        status: XHR.status,
+        statusText: XHR.statusText,
         headers: responseHeaders,
         config,
-        request
+        request: XHR
       }
       resolve(response)
     }
@@ -32,11 +33,11 @@ function xhr(config: AxiosRequestConfig): AxiosPromise {
       if (data === null && name.toLocaleLowerCase() === 'content-type') {
         delete headers[name]
       } else {
-        request.setRequestHeader(name, headers[name])
+        XHR.setRequestHeader(name, headers[name])
       }
     })
 
-    request.send(data)
+    XHR.send(data)
   })
 }
 
