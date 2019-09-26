ef766ef839ff07a945c18fcd9211d447e15a1ca5
shiswq@hletong.com <shiswq@hletong.com>
2019-09-26 14:42:16 +0800

    feat: 设置请求时间,超过这个时间请求终止

--- src/xhr.ts
+++ src/xhr.ts
@@ -3,11 +3,15 @@ import { parseHeaders } from './helpers/headers'
 
 function xhr(config: AxiosRequestConfig): AxiosPromise {
   return new Promise((resolve, reject) => {
-    const { url, method = 'get', data = null, headers = {}, responseType } = config
+    const { url, method = 'get', data = null, headers = {}, responseType, timeout } = config
     const XHR = new XMLHttpRequest()
 
     if (responseType) {
       XHR.responseType = responseType
+    }
+
+    if (timeout) {
+      XHR.timeout = timeout
     }
 
     XHR.open(method.toLocaleUpperCase(), url, true)
@@ -16,6 +20,11 @@ function xhr(config: AxiosRequestConfig): AxiosPromise {
       if (XHR.readyState !== 4) {
         return
       }
+
+      if (XHR.status === 0) {
+        return
+      }
+
       const responseHeaders = parseHeaders(XHR.getAllResponseHeaders())
       const responseData = responseType !== 'text' ? XHR.response : XHR.responseText
       const response: AxiosResponse = {
@@ -26,7 +35,21 @@ function xhr(config: AxiosRequestConfig): AxiosPromise {
         config,
         request: XHR
       }
-      resolve(response)
+      handleResponse(response)
+      // 如果不理解封装,可以用下面注释的代码,这样理解更容易
+      // if(response.status >=200 && response.status <= 300){
+      //   resolve(response);
+      // }else{
+      //   reject(new Error(`Request failed with statuscode ${response.status}`))
+      // }
+    }
+
+    XHR.onerror = function handleError() {
+      reject(new Error('Network Error'))
+    }
+
+    XHR.ontimeout = function handleTimeout() {
+      reject(new Error(`Timeout of ${timeout} ms exceeded`))
     }
 
     Object.keys(headers).forEach(name => {
@@ -38,6 +61,17 @@ function xhr(config: AxiosRequestConfig): AxiosPromise {
     })
 
     XHR.send(data)
+    /***
+     * 函数内部定义函数,作用域也被限定在函数内,只是为了单纯的封装一下
+     * handleResponse
+     * ***/
+    function handleResponse(response: AxiosResponse): void {
+      if (response.status >= 200 && response.status <= 300) {
+        resolve(response)
+      } else {
+        reject(new Error(`Request failed with statuscode ${response.status}`))
+      }
+    }
   })
 }
 
