2fce1e73d143f5c59a5410922051ad7c8e39e679
shiswq@hletong.com <shiswq@hletong.com>
2019-10-11 16:14:10 +0800

    fix: put,post,patch needs data

--- src/types/index.ts
+++ src/types/index.ts
@@ -52,11 +52,11 @@ export interface AxiosPromise extends Promise<AxiosResponse> {}
  * @interface 用于处理错误信息
  */
 export interface AxiosError extends Error {
-    config: AxiosRequestConfig
-    code?:string | null
-    request?:any
-    response?:AxiosResponse
-    isAxiosError:boolean
+  config: AxiosRequestConfig
+  code?: string | null
+  request?: any
+  response?: AxiosResponse
+  isAxiosError: boolean
 }
 
 /**
@@ -66,27 +66,26 @@ export interface Axios {
   // 能分清interface中的函数类型和类类型的方法吗？因为混合类型经常碰到这种问题
   // 这里是类类型的方法,专门用于约束class类的方法
   // 能分清主要看是否命名,命名了就是类类型的方法
-  request(config:AxiosRequestConfig):AxiosPromise
+  request(config: AxiosRequestConfig): AxiosPromise
 
-  get(url:string,config?:AxiosRequestConfig):AxiosPromise
+  get(url: string, config?: AxiosRequestConfig): AxiosPromise
 
-  delete(url:string,config?:AxiosRequestConfig):AxiosPromise
+  delete(url: string, config?: AxiosRequestConfig): AxiosPromise
 
-  head(url:string,config?:AxiosRequestConfig):AxiosPromise
+  head(url: string, config?: AxiosRequestConfig): AxiosPromise
 
-  options(url:string,config?:AxiosRequestConfig):AxiosPromise
+  options(url: string, config?: AxiosRequestConfig): AxiosPromise
 
-  post(url:string,data:any,config?:AxiosRequestConfig):AxiosPromise
+  post(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
 
-  put(url:string,config?:AxiosRequestConfig):AxiosPromise
+  put(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
 
-  patch(url:string,config?:AxiosRequestConfig):AxiosPromise
-
+  patch(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
 }
 
 /**
  * @interface AxiosInstance 本来只是专门约束的一个函数,由于继承了Axios,函数又有了自己的属性方法便扩展成混合类型,这种分开定义再继承方式很好 //https://typescript.bootcss.com/interfaces.html
  */
-export interface AxiosInstance extends Axios{
-  (config:AxiosRequestConfig):AxiosPromise
+export interface AxiosInstance extends Axios {
+  (config: AxiosRequestConfig): AxiosPromise
 }
