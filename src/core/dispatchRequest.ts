29c767e7a57ff9ba853b36408cfc93847e497b10
shiswq@hletong.com <shiswq@hletong.com>
2020-01-03 14:41:28 +0800

    feat: 🎸 重构transform函数,代码清晰很多

--- src/core/dispatchRequest.ts
+++ src/core/dispatchRequest.ts
@@ -3,6 +3,7 @@ import xhr from './xhr'
 import { buildURL } from '../helpers/url'
 import { transfromRequest, transfromResponse } from '../helpers/data'
 import { processHeaders,flattenHeaders } from '../helpers/headers'
+import transform from './transform'
 
 /**
  * @description axios函数的主体,调用xhr函数,实现浏览器ajax功能
@@ -22,8 +23,7 @@ function dispatchRequest(config: AxiosRequestConfig): AxiosPromise {
  */
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformURL(config)
-  config.headers = transformHeaders(config)
-  config.data = transformRequestData(config)
+  config.data = transform(config.data,config.headers,config.transformRequest)
   config.headers = flattenHeaders(config.headers,config.method!)
 }
 
@@ -37,22 +37,8 @@ function transformURL(config: AxiosRequestConfig): string {
   return buildURL(url!, params)
 }
 
-/**
- * @description 调用buildURL函数,对data进行处理
- * @param {AxiosRequestConfig} config
- */
-
-function transformRequestData(config: AxiosRequestConfig): any {
-  const { data } = config
-  return transfromRequest(data)
-}
-function transformHeaders(config: AxiosRequestConfig): any {
-  const { headers = {}, data } = config
-  return processHeaders(headers, data)
-}
-
 function transformResponseData(res: AxiosResponse): AxiosResponse {
-  res.data = transfromResponse(res.data)
+  res.data = transform(res.data,res.headers,res.config.transformResponse)
   return res
 }
 
