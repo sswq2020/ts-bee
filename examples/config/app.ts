eb0c1672ff0e12d8f70cba2dd4a73655d7c1959b
shiswq@hletong.com <shiswq@hletong.com>
2020-01-03 19:02:47 +0800

    test: 💍 transform测试

--- examples/config/app.ts
+++ examples/config/app.ts
@@ -1,15 +1,38 @@
-import axios from '../../src/index'
+import axios, { AxiosTransformer } from '../../src/index'
 import qs from 'qs'
 axios.defaults.headers.common['test2'] = 123
 
 // tslint:disable-next-line: no-floating-promises
 axios({
-  url:"/config/post",
-  method:'post',
-  data:qs.stringify({a:1}),
-  headers:{
-    test:'321'
+  url: "/config/post",
+  method: 'post',
+  data: { a: 1 },
+  headers: {
+    test: '321'
   }
-}).then((res)=>{
+}).then((res) => {
+  console.log(res.data)
+})
+
+// tslint:disable-next-line: no-floating-promises
+axios({
+  transformRequest: [
+    function (data) {
+      return qs.stringify(data)
+    }, ...(axios.defaults.transformRequest as AxiosTransformer[])
+  ],
+  transformResponse: [
+    ...(axios.defaults.transformResponse as AxiosTransformer[]), function (data) {
+      if (typeof data === 'object') {
+        data.b = 2
+      }
+    }
+  ],
+  url: "/config/post",
+  method: 'post',
+  data: {
+    a:9864234
+  }
+}).then((res) => {
   console.log(res.data)
 })
