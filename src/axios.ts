a35a987e2af20cf5656f8ef8046d694e5145c22f
shiswq@hletong.com <shiswq@hletong.com>
2020-02-01 21:34:18 +0800

    feat: 🎸 axios实例上添加3个静态方法

--- src/axios.ts
+++ src/axios.ts
@@ -3,6 +3,9 @@ import Bee from './core/Axios'
 import { extend } from './helpers/util'
 import {defaults} from './default'
 import mergeConfig from './core/mergeConfig'
+import CancelToken from './cancel/CancelToken'
+import Cancel, {isCancel} from './cancel/Cancel'
+
 
 function createInstance(config: AxiosRequestConfig): AxiosStatic {
   const context = new Bee(config)
@@ -16,5 +19,10 @@ const axios = createInstance(defaults)
 axios.create = function(config){
   return createInstance(mergeConfig(defaults,config))
 }
+
+axios.CancelToken = CancelToken
+axios.Cancel = Cancel
+axios.isCancel = isCancel
+
 
 export default axios
